bb314ad8e91af3986facd390251d5c49cada5554
Brittany Javalera <brittjavalera@gmail.com>
2020-08-08 12:37:38 -0700

    removed thunk and createStore imports in index.js since they are in separate store.js file

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore, applyMiddleware } from 'redux'
-import { thunk } from 'redux-thunk'
+
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
