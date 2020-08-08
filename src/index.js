4dac89af7065a1d94b74894ccac17d8d37630de2
Brittany Javalera <brittjavalera@gmail.com>
2020-08-08 12:35:23 -0700

    added import for thunk, provider, createStore, applyMiddleware in index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import { createStore, applyMiddleware } from 'redux'
+import { thunk } from 'redux-thunk'
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 
