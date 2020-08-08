d08e501a40a85f8616b43726f2b7ce488dbc5445
Brittany Javalera <brittjavalera@gmail.com>
2020-08-08 12:49:25 -0700

    creaded userReducer and import in store.js

--- src/store.js
+++ src/store.js
@@ -1,7 +1,9 @@
 import thunk from 'redux-thunk'
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
+import userReducer from './reducers/user'
 
 const reducer = combineReducers({
+    user: userReducer
     // put reducers here for each action
 })
 
