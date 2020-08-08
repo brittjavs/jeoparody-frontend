b3e7285fad2cb3c9b4a3535f55cae5e8daf2f6db
Brittany Javalera <brittjavalera@gmail.com>
2020-08-08 13:42:01 -0700

    created login component with a form and a handleSubmit and handleOnChange functions. Import Login inside of App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Login from "./components/Login.js"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
       <h5>JeoParody Test</h5>
+      <Login />
       </header>
     </div>
   );
---
+++ src/components/Login.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {login} from '../actions/currentUser.js'
+
+class Login extends React.Component {
+    
+    state = {
+        username: '',
+        password: ''
+    }
+
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.login(this.state)
+        this.setState({
+            username: '',
+            password: ''
+        })
+        this.props.history.push('/')
+    }
+
+    render(){
+        return(
+            <div className="login-form">
+                <form onSubmit={this.handleSubmit}> 
+                    <label>Username: </label>
+                    <input type="text" name="username" value={this.state.username} onChange={this.handleChange} required/>
+                    <br />
+                    <label>Password: </label>
+                    <input type="text" name="password" value={this.state.password} onChange={this.handleChange} required/>
+                    <br />
+                    <input type="submit" value="Log In"/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default connect(null, { login })(Login)
