aaeb416005d286115f1f3ec066ce9b25398bdb4d
khris22 <khrispunzalan@gmail.com>
2020-08-14 19:45:28 -0400

    deleted NavLink on Logout

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,38 +1,16 @@
 import React from 'react'
-<<<<<<< HEAD
-import { NavLink } from 'react-router-dom'
-=======
 import Login from '../components/Login'
 import Signup from '../components/Signup'
 import Logout from '../components/Logout'
 import { connect } from 'react-redux'
 import { Route, Switch, NavLink } from 'react-router-dom'
->>>>>>> 3464844fa7c38de8c09bf7b8f7e21dc48ade60b9
 
 class NavBar extends React.Component {
     render(){
         return(
-<<<<<<< HEAD
-            <div>
-            <button><NavLink exact activeClassName="active" to="/login">Login</NavLink></button>
-            <NavLink exact activeClassName="active"to="/signup">Signup</NavLink>
-
-            {/* Show only log out when current user is logged in */}
-            <NavLink exact activeClassName="active" to="/logout">Logout</NavLink>
-
-
-
-=======
             this.props.currentUser ? 
             <div className="logged-in-nav">
-                <NavLink to = "/logout">Log Out</NavLink>
-                    <div>
-                        {/* <Logout /> */}
-                        <Switch>
-                        <Route exact path='/logout' component={Logout}/>
-                        </Switch>
-                        
-                    </div>
+                        <Logout />
             </div>
             :
             <div className="auth-nav">
@@ -48,7 +26,6 @@ class NavBar extends React.Component {
                 <Route exact path='/signup' component={Signup}/>
                 </Switch>
                 </div>
->>>>>>> 3464844fa7c38de8c09bf7b8f7e21dc48ade60b9
             </div>
             
         )
