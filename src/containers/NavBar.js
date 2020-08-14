6894074c18d9d4fe38b36c0bb929b074b73e9ccd
khris22 <khrispunzalan@gmail.com>
2020-08-13 21:22:20 -0400

    add Route to NavBar

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -3,14 +3,20 @@ import React from 'react'
 import Login from '../components/Login'
 import Signup from '../components/Signup'
 import Logout from '../components/Logout'
+import { Route, Switch } from 'react-router-dom'
 
 class NavBar extends React.Component {
     render(){
         return(
             <div>
-            <Login />
-            <Signup />
-            <Logout />
+            <Switch>
+                {/* <Login /> */}
+                <Route exact path='/login' component={Login}/>
+                {/* <Signup /> */}
+                <Route exact path='/signup' component={Signup}/>
+                {/* <Logout /> */}
+                <Route exact path='/logout' component={Logout}/>
+            </Switch>
             </div>
             
         )
@@ -18,3 +24,4 @@ class NavBar extends React.Component {
 }
 
 export default NavBar
+// Do we need to add withRouter here?
