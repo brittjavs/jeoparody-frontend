737ae8c0f1f410d48efa519856968e998d372818
khris22 <khrispunzalan@gmail.com>
2020-08-14 17:34:52 -0400

    move route to App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import NavBar from "./containers/NavBar"
 import { connect } from 'react-redux'
 import { getCurrentUser } from "./actions/currentUser"
+import { Route, Switch } from 'react-router-dom'
+import Login from './components/Login'
+import Signup from './components/Signup'
+import Logout from './components/Logout'
+
 
 class App extends React.Component {
 
@@ -16,6 +21,11 @@ class App extends React.Component {
         <header className="App-header">
           <h5>JeoParody Test</h5>
           <NavBar />
+          <Switch>
+            <Route exact path='/login' component={Login}/>
+            <Route exact path='/signup' component={Signup}/>
+            <Route exact path='/logout' component={Logout}/>
+          </Switch>
         </header>
       </div>
     )
