1ba1576f9ea2f8c289c17a7027f68ead22fa5b1c
Brittany Javalera <brittjavalera@gmail.com>
2020-08-15 22:00:26 -0700

    created categories container

---
+++ src/containers/Categories.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Categories extends React.Component {
+    render(){
+        return(
+            <div className="categories-container">
+                Categories go here
+            </div>
+        )
+    }
+}
+
+export default Categories
